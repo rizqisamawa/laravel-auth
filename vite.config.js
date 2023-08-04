ec38d1a7c6bbe7b2ad19372f355a89f995f07927
rizqisamawa <rizqisamawa11@gmail.com>
2023-08-04 13:25:23 +0700

    chore: hot reload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: ["resources/routes/**", "routes/**", "resources/views/**"],
         }),
     ],
 });
+
+/*
+   -  dont forget to add script in tag head @vite('resources/js/app.js')
+   -  this script just for development
+*/
